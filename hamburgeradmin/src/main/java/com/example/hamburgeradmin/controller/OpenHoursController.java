bb8c0b5777194f42fdd05d2f268c146a06b56f55
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:06:36 -0600

    refactor(hamburger-api): refactor OpenHoursController

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/controller/OpenHoursController.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/controller/OpenHoursController.java
@@ -1,50 +1,46 @@
 package com.example.hamburgeradmin.controller;
 
+import com.example.hamburgeradmin.dto.LocationDTO;
+import com.example.hamburgeradmin.model.Location;
 import com.example.hamburgeradmin.model.OpenHours;
 import com.example.hamburgeradmin.services.OpenHoursServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+/**
+ * @author Yash Dubey
+ * <p>
+ * This class exposes resources to interact with Open Hours in Location entity
+ */
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 @Log4j2
+@Tag(name = "OpenHours Controller", description = "Resources related to Open Hours in the Location entity")
 public class OpenHoursController {
-    @Autowired
-    OpenHoursServices openHoursServices;
+    private final OpenHoursServices openHoursServices;
 
-    @GetMapping("/open-hours")
-    public ResponseEntity<List<OpenHours>> getAllOpenHours(@RequestParam(required = false) String name) {
-        return openHoursServices.getAllOpenHours(name);
-    }
-
-    @GetMapping("/open-hours/{id}")
-    public ResponseEntity<OpenHours> getOpenHoursById(@PathVariable("id") String id) {
-        return openHoursServices.getOpenHoursById(id);
-    }
-
-    @PostMapping("/open-hours")
-    public ResponseEntity<OpenHours> createOpenHours(@RequestBody OpenHours openHours) {
-        return openHoursServices.createOpenHours(openHours);
-    }
-
-    @PutMapping("/open-hours/{id}")
-    public ResponseEntity<OpenHours> updateOpenHours(@PathVariable("id") String id, @RequestBody OpenHours openHours) {
-        return openHoursServices.updateOpenHours(id, openHours);
-    }
-
-    @DeleteMapping("/open-hours/{id}")
-    public ResponseEntity<HttpStatus> deleteOpenHours(@PathVariable("id") String id) {
-        return openHoursServices.deleteOpenHours(id);
-    }
-
-    @DeleteMapping("/open-hours")
-    public ResponseEntity<HttpStatus> deleteAllOpenHours() {
-        return openHoursServices.deleteAllOpenHours();
+    @Operation(summary = "Update Open Hours for a Location by id in the Location Entity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Open Hours for Location updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OpenHours.class)) }),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = @Content) })
+    @PutMapping("/hours/{id}")
+    public ResponseEntity updateOpenHours(@PathVariable("id") String id, @RequestBody Location openHours) {
+        LocationDTO updatedHours = openHoursServices.updateOpenHours(id, openHours);
+        if(updatedHours != null) {
+            return ResponseEntity.ok(updatedHours);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
-
