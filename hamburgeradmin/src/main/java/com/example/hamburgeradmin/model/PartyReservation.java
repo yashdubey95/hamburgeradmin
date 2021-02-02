b96448262d929b688899b24179c36b3a8541ebf8
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:32:52 -0600

    refactor(hamburger-api): refactor PartyReservation model

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/PartyReservation.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/PartyReservation.java
@@ -1,18 +1,31 @@
 package com.example.hamburgeradmin.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "party_reservations")
 @Data
 public class PartyReservation {
     @Id
     private String reservationId;
     private String customerName;
-    private String partyType;
-    private int peopleCount;
+    private PartyType partyType;
+    private Integer peopleCount;
     private String table;
-    private String date;
-    private String time;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate date;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime endTime;
 }
