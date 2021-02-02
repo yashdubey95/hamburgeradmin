d563d9e474b553448059ffd99ef978a7440201aa
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:20:55 -0600

    refactor(hamburger-api):  refactor ExecutionTimeInterceptor

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/interceptor/ExecutionTimeInterceptor.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/interceptor/ExecutionTimeInterceptor.java
@@ -3,22 +3,48 @@ package com.example.hamburgeradmin.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.hamburgeradmin.model.APIDetails;
+import com.example.hamburgeradmin.repository.InterceptorDataRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
 import lombok.extern.log4j.Log4j2;
 
+import java.time.LocalDate;
+
+/**
+ * @author Yash Dubey
+ * <p>
+ * This class encapsulates the implementation of a Request Interceptor
+ */
+@AllArgsConstructor
 @Component
 @Log4j2
-public class ExecutionTimeInterceptor implements HandlerInterceptor {
+public class ExecutionTimeInterceptor implements HandlerInterceptor  {
+
+    private InterceptorDataRepository interceptorDataRepository;
     @Override
     public void afterCompletion(HttpServletRequest request,
                                 HttpServletResponse response, Object object, Exception arg3)
             throws Exception {
-
         long startTime = (Long) request.getAttribute("startTime");
+        LocalDate timeStamp = (LocalDate) request.getAttribute("timeStamp");
         long endTime = System.currentTimeMillis();
-        log.info("Request URL: {}", request.getRequestURL());
-        log.info("Total Time Taken: {}", (endTime - startTime));
+        long time = (endTime - startTime);
+        log.info("End time: {}", endTime);
+        log.info("TimeStamp {}", timeStamp);
+        log.info("Request Name: {}", request.getMethod());
+        log.info("Request URL: {}", request.getRequestURL().toString());
+        log.info("Total Time Taken: {} ms", time);
+
+        APIDetails apiDetails = new APIDetails();
+        apiDetails.setReqName(request.getMethod());
+        apiDetails.setReqUrl(request.getRequestURL().toString());
+        apiDetails.setReqTimeStamp(timeStamp);
+        apiDetails.setReqExecTime(time);
+        interceptorDataRepository.save(apiDetails);
+
     }
 
 
@@ -27,7 +53,10 @@ public class ExecutionTimeInterceptor implements HandlerInterceptor {
                              HttpServletResponse response, Object object) throws Exception {
 
         long startTime = System.currentTimeMillis();
+        LocalDate timeStamp = LocalDate.now();
+        request.setAttribute("timeStamp", timeStamp);
         request.setAttribute("startTime", startTime);
         return true;
     }
+
 }
