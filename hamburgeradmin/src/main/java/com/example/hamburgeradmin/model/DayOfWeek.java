5ff6021ac448872aa74912088438ee952501fb5f
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:24:19 -0600

    feat(hamburger-api): add DayOfWeek model

---
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/DayOfWeek.java
@@ -0,0 +1,8 @@
+package com.example.hamburgeradmin.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
