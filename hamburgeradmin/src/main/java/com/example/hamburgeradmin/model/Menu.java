0ecec1b695506bb8bd532af59e0002c4742642b8
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:30:36 -0600

    refactor(hamburger-api): refactor Menu model

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Menu.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Menu.java
@@ -1,9 +1,13 @@
 package com.example.hamburgeradmin.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "menu")
 @Data
 public class Menu extends Category{
