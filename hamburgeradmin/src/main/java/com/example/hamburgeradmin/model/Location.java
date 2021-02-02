e28e3519759c5f4bef6f24c0eb87bdd35ebf04ab
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:28:06 -0600

    refactor(hamburger-api): add fields to model

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Location.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Location.java
@@ -1,9 +1,15 @@
 package com.example.hamburgeradmin.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "locations")
 @Data
 public class Location {
@@ -15,4 +21,5 @@ public class Location {
     private String address;
     private String phone;
     private Boolean active;
+    private List<OpenHours> openHours;
 }
