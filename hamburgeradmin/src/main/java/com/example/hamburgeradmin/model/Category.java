08d61e00594823157dd5a772e1206f4f5989705e
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-01 18:29:23 -0600

    refactor(hamburger-api): refactor Category model

--- hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Category.java
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/model/Category.java
@@ -1,7 +1,11 @@
 package com.example.hamburgeradmin.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Category {
     private String category;
