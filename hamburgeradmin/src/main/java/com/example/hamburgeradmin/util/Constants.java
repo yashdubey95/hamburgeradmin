51bba87ce342f20135f73284fc799d69d2e75e73
Yash Dubey <47196454+yashdubey95@users.noreply.github.com>
2021-02-05 17:11:14 -0600

    feat(hamburger-api):  add Constants file

---
+++ hamburgeradmin/src/main/java/com/example/hamburgeradmin/util/Constants.java
@@ -0,0 +1,10 @@
+package com.example.hamburgeradmin.util;
+
+public class Constants {
+    public Constants() {
+    }
+
+    public static final String BOOTSTRAP_SERVERS_CONFIG = "127.0.0.1:9092";
+    public static final String GROUP_ID_CONFIG = "group_json";
+    public static final String TOPIC = "Kafka_Example";
+}
